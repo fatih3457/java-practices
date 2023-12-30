7bda64a97535172d30e1f338a8829f24822fbcdb
Fatih Özcan <58815565+fatih3457@users.noreply.github.com>
2023-12-30 12:08:16 +0300

    Update Main.java

--- finding-even-numbers/src/Main.java
+++ finding-even-numbers/src/Main.java
@@ -1,19 +1,27 @@
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        int k;
+        int number, k = 0, total = 0;
         Scanner inp = new Scanner(System.in);
 
         System.out.print("Enter Number: ");
-        k = inp.nextInt();
+        number = inp.nextInt();
 
-        for (int i = 0; i<=k; i++) {
-            if (i % 3 == 0 && i % 4 == 0){
-                System.out.println(i +" ");
+        for (int i = 0; i<=number; i++) {
+            if (i % 3 == 0 || i % 4 == 0){
+                total = total + i;
+                k++;
             }
         }
 
+        if (k > 0) {
+            double avarage = total / k;
+            System.out.println("0'dan " + number + "kadar olan 3 ve 4'e tam bölünen sayıların ortalaması: " + avarage);
+        } else {
+            System.out.println("Bu aralıkta 3 ve 4'e tam bölünen herhangi bir sayı bulunamadı.");
+        }
     }
 }
